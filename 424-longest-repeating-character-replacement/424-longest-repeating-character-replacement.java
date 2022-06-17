37a16adb4fa4db20713f82e042113409c2d57736
YomnaHeshamFekry <yomna.heshamyh@hotmail.com>
2022-06-17 10:37:28 +0200

    Time: 21 ms (45.68%), Space: 45.3 MB (22.89%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -7,18 +7,12 @@ class Solution {
         int right = 0;
         int length = 0;
         int maxFreq = 0;
-        int nextMaxFreq = 0;
         int n = s.length();
         
         while(left < n && right < n) {
             int charFreq = charFreqs.getOrDefault(s.charAt(right), 0);
             charFreq++;
-            if (charFreq > maxFreq) {
-                maxFreq = charFreq;
-            }
-            else if (charFreq > nextMaxFreq) {
-                nextMaxFreq = charFreq;
-            }
+            maxFreq = Math.max(maxFreq, charFreq);
             
             length = right-left+1;
             if (length - maxFreq <= k) {
@@ -27,11 +21,8 @@ class Solution {
                 right++;
             } 
             else {
-                length--;
-                
                 charFreq = charFreqs.get(s.charAt(left));
                 charFreqs.put(s.charAt(left), --charFreq);
-                // maxFreq = Math.max(nextMaxFreq, --maxFreq);
                 left++;
             }
         }
